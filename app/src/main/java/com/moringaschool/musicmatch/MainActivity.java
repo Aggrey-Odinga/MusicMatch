469b245790463a8d1bd9bd01caa6c0cd7396c05a
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-05-29 15:38:10 +0300

    Add button vaiable to java logic

--- app/src/main/java/com/moringaschool/musicmatch/MainActivity.java
+++ app/src/main/java/com/moringaschool/musicmatch/MainActivity.java
@@ -3,12 +3,18 @@ package com.moringaschool.musicmatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+//    declare button view variable
+    private Button mButtontoptenartists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        setting the variable
+
+        mButtontoptenartists = (Button)findViewById(R.id.buttontoptenartists);
     }
 }

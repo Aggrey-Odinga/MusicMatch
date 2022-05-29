1a194b613e3e54c769bee500a07479554836d7af
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-05-29 19:13:22 +0300

    Add arraylist of artists

--- app/src/main/java/com/moringaschool/musicmatch/ArtistsActivity.java
+++ app/src/main/java/com/moringaschool/musicmatch/ArtistsActivity.java
@@ -3,12 +3,26 @@ package com.moringaschool.musicmatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ArtistsActivity extends AppCompatActivity {
+
+    private String [] Artists = new String[] {"Sauti Sol",
+            "Nviiri The Storyteller", "Otile Brown",
+            "Nyashinski", "Bensoul", "Wakadinali",
+            "BURUKLYN BOYZ", "H_art the Band",
+            "Mejja", "Khaligraph Jones"};
+
+//    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artists);
+
+//        mListView = (ListView) findViewById(R.id.listView);
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+//        mListView.setAdapter(adapter);
     }
 }

67ba5d66152feb2c9cff03624fb1a6158f236fb1
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-05-29 15:44:50 +0300

    Add a toast message to button when clicked

--- app/src/main/java/com/moringaschool/musicmatch/MainActivity.java
+++ app/src/main/java/com/moringaschool/musicmatch/MainActivity.java
@@ -3,7 +3,9 @@ package com.moringaschool.musicmatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 //    declare button view variable
@@ -16,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
 //        setting the variable
 
         mButtontoptenartists = (Button)findViewById(R.id.buttontoptenartists);
+        mButtontoptenartists.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
     }
 }

07c8448d0b184c7e419298010256fb0a637a9960
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-05-29 19:22:34 +0300

    Add initialization method for listview

--- app/src/main/java/com/moringaschool/musicmatch/ArtistsActivity.java
+++ app/src/main/java/com/moringaschool/musicmatch/ArtistsActivity.java
@@ -8,20 +8,19 @@ import android.widget.ListView;
 
 public class ArtistsActivity extends AppCompatActivity {
 
+    private ListView mListView;
     private String [] Artists = new String[] {"Sauti Sol",
             "Nviiri The Storyteller", "Otile Brown",
             "Nyashinski", "Bensoul", "Wakadinali",
             "BURUKLYN BOYZ", "H_art the Band",
             "Mejja", "Khaligraph Jones"};
 
-//    private ListView mListView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artists);
 
-//        mListView = (ListView) findViewById(R.id.listView);
+        mListView = (ListView) findViewById(R.id.listView);
 //        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
 //        mListView.setAdapter(adapter);
     }

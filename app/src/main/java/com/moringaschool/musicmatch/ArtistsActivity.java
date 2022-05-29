9e0b880a54f5791ae2e041a3663ca7a3932cae9d
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-05-29 19:46:05 +0300

    Add Toast message on item click

--- app/src/main/java/com/moringaschool/musicmatch/ArtistsActivity.java
+++ app/src/main/java/com/moringaschool/musicmatch/ArtistsActivity.java
@@ -3,8 +3,12 @@ package com.moringaschool.musicmatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ArtistsActivity extends AppCompatActivity {
 
@@ -23,5 +27,14 @@ public class ArtistsActivity extends AppCompatActivity {
         mListView = (ListView) findViewById(R.id.listView);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Artists);
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String Artists = ((TextView)view).getText().toString();
+                Toast.makeText(ArtistsActivity.this, Artists, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        };
     }
-}

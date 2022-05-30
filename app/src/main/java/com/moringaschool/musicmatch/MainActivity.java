77287f2c60f5a45b9690121c0a20b162598870d8
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-05-30 13:16:49 +0300

    Add bug fixes and indendation

--- app/src/main/java/com/moringaschool/musicmatch/MainActivity.java
+++ app/src/main/java/com/moringaschool/musicmatch/MainActivity.java
@@ -6,39 +6,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-//    declare button view variable
-//    view binding to locate button easily
-//    private Button mButtontoptenartists;
+
     @BindView(R.id.buttontoptenartists) Button mButtontoptenartists;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.buttonlisttrendingsongs)
+    Button mbuttonlisttrendingsongs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        implement butterknife
         ButterKnife.bind(this);
-//        setting the variable
-
-//        mButtontoptenartists = (Button)findViewById(R.id.buttontoptenartists);
-//      implement a toast msg
-//        implement intent tot take us to new page
 
         mButtontoptenartists.setOnClickListener(this);
+        mbuttonlisttrendingsongs.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
-        if(v == mButtontoptenartists){
-            //                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(MainActivity.this, ArtistsActivity.class);
+        if(v == mbuttonlisttrendingsongs) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, SongsActivity.class);
+            intent.putExtra("location", location);
             startActivity(intent);
-
         }
-
+        if(v == mButtontoptenartists){
+             Intent intent = new Intent(MainActivity.this, ArtistsActivity.class);
+            startActivity(intent);
+        }
     }
 }

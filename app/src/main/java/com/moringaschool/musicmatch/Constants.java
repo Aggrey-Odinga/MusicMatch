600a8fdd3dc7e7fa13f9bca8dfa1165b73768569
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-06-06 12:58:36 +0300

    Add constants class containing reference to our credentials

---
+++ app/src/main/java/com/moringaschool/musicmatch/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.musicmatch;
+
+public class Constants {
+    public static final String MUSIXMATCH_BASE_URL = "http://api.musixmatch.com/ws/1.1/";
+    public static final String API_KEY = BuildConfig.API_KEY;
+}

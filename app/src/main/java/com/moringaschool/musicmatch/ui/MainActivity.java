8b6c0305d88fc4346d5ca669d197696e9329c3e2
Aggrey-Odinga <aggrey.odinga@student.moringaschool.com>
2022-06-14 18:29:57 +0300

    Add firebase database to store user interaction history

--- app/src/main/java/com/moringaschool/musicmatch/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/musicmatch/ui/MainActivity.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.moringaschool.musicmatch.R;
+import com.moringaschool.musicmatch.network.Constants;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private DatabaseReference mSearchedArtistReference;
 
     @BindView(R.id.artistEditText)
     EditText martistEditText;
@@ -26,6 +34,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mSearchedArtistReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_ARTIST);
+
+        mSearchedArtistReference.addValueEventListener(new ValueEventListener() { //attach listener
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) { //something changed!
+                for (DataSnapshot artistSnapshot : dataSnapshot.getChildren()) {
+                    String artist = artistSnapshot.getValue().toString();
+                    Log.d("Artists update", "artist: " + artist); //log
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { //update UI here if error occurred.
+
+            }
+        });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
@@ -35,10 +63,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        if(v ==  mButtontoptenartists){
             String artist = martistEditText.getText().toString();
+            saveArtistToFirebase(artist);
             Intent intent = new Intent(MainActivity.this, ArtistsListActivity.class);
             intent.putExtra("artist", artist);
             startActivity(intent);
+        }
+    }
+    public void saveArtistToFirebase(String artist) {
+        mSearchedArtistReference.push().setValue(artist);
     }
     public void onLaunchWebPagebuttonClicked (View view) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/musixmatch/musixmatch-sdk"));
